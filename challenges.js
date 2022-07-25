5e097ea5fe4f97a09cbe92d64bc2b11ed665627a
chloewigsten <chloewigsten@gmail.com>
2022-07-25 15:43:31 -0400

    solved challenge 01 addOne function

--- challenges.js
+++ challenges.js
@@ -34,9 +34,10 @@ addOne(-5) //=> -4
 -----------------------------------------------------------------*/
 // Your solution for 01-addOne here:
 
-
-
-
+function addOne(num) {
+  return num + 1;
+}
+console.log(addOne(11))
 
 /*-----------------------------------------------------------------
 Challenge: 02-addTwoNumbers
